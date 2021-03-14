044dd9b3abf353f506dad9468914e611d2165371
alexanderson0708 <sonalexander0708@gmail.com>
2021-03-15 01:15:32 +0500

    fix: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = false
+  str = str.split('');
+  brackets(str, bracketsConfig)  
+  function brackets(str){
+    if(str.length == 0){
+      a = true;
+      } else {
+        for (let i = 0; i < str.length; i++){
+          for (let j = 0; j < bracketsConfig.length; j++){
+            if(str[i] == bracketsConfig[j][0] && str[i+1] == bracketsConfig[j][1]){
+              str.splice(i, 2);
+              brackets(str);
+            }
+          }
+        }
+      }
+    }
+    return a;
 }
